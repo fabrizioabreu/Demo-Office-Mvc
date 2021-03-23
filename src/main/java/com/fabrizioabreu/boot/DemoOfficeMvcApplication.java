9ce42fc9dc49614218e2970da9b4aac5fea97588
Fabrizio Abreu <fabrizioabreu@live.com>
2021-03-23 11:28:37 -0300

    Projeto Criado

---
+++ src/main/java/com/fabrizioabreu/boot/DemoOfficeMvcApplication.java
@@ -0,0 +1,13 @@
+package com.fabrizioabreu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOfficeMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOfficeMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrizioabreu/boot/DemoOfficeMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrizioabreu.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOfficeMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
