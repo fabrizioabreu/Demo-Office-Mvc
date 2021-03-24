9aa2cba3a9850069b9d501a3c46d4325b6d2ccc1
Fabrizio Abreu <fabrizioabreu@live.com>
2021-03-24 17:54:39 -0300

    EDITANDO Cargos - Controller e View

--- src/main/java/com/fabrizioabreu/boot/web/controller/CargoController.java
+++ src/main/java/com/fabrizioabreu/boot/web/controller/CargoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -46,5 +47,18 @@ public class CargoController {
 	@ModelAttribute("departamentos")
 	public List<Departamento> listaDeDepartamentos(){
 		return departamentoService.buscarTodos();
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id)); // Buscando Departamento que vai ser editado
+		return "cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success", "Registro atualizado com sucesso.");
+		return "redirect:/cargos/cadastrar";
 	}
 }
