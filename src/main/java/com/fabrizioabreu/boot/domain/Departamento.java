b89f2a7b5b58bf19783e879d9dde92c1a080213e
Fabrizio Abreu <fabrizioabreu@live.com>
2021-03-25 16:10:05 -0300

    Validando formulários de Cargo e Departamento

--- src/main/java/com/fabrizioabreu/boot/domain/Departamento.java
+++ src/main/java/com/fabrizioabreu/boot/domain/Departamento.java
@@ -3,12 +3,16 @@ package com.fabrizioabreu.boot.domain;
 import java.util.List;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "DEPARTAMENTOS")
 public class Departamento extends AbstractEntity<Long> {
 
+	@NotBlank(message = "Informe um nome.")
+	@Size(min = 3, max = 60, message = "O nome do departamento de ter entre {min} e {max} caracteres.")
 	@Column(name = "nome", nullable = false, unique = true, length = 60)
 	private String nome;
 	
@@ -20,7 +24,7 @@ public class Departamento extends AbstractEntity<Long> {
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
+		this.nome = nome.trim();
 	}
 
 	public List<Cargo> getCargos() {
