4a05ff023197f8387a11faa61b00d24ebf2f64cd
Fabrizio Abreu <fabrizioabreu@live.com>
2021-03-25 09:28:17 -0300

    INSERINDO Funcionários

--- src/main/java/com/fabrizioabreu/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/fabrizioabreu/boot/web/controller/FuncionarioController.java
@@ -1,20 +1,55 @@
 package com.fabrizioabreu.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.fabrizioabreu.boot.domain.Cargo;
+import com.fabrizioabreu.boot.domain.Funcionario;
+import com.fabrizioabreu.boot.domain.UF;
+import com.fabrizioabreu.boot.service.CargoService;
+import com.fabrizioabreu.boot.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@Autowired
+	private CargoService cargoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
 
 	@GetMapping("/listar")
 	public String listar() {
 		return "/funcionario/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
 	}
 }
