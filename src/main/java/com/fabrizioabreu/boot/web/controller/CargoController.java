62c6db44ebae6f60712894450dce866ec90f21ac
Fabrizio Abreu <fabrizioabreu@live.com>
2021-03-26 20:47:03 -0300

    Preparando o Controller e Cargo Lista.html para navegar entre a paginação

--- src/main/java/com/fabrizioabreu/boot/web/controller/CargoController.java
+++ src/main/java/com/fabrizioabreu/boot/web/controller/CargoController.java
@@ -1,6 +1,7 @@
 package com.fabrizioabreu.boot.web.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fabrizioabreu.boot.domain.Cargo;
 import com.fabrizioabreu.boot.domain.Departamento;
 import com.fabrizioabreu.boot.service.CargoService;
 import com.fabrizioabreu.boot.service.DepartamentoService;
+import com.fabrizioabreu.boot.util.PaginacaoUtil;
 
 @Controller
 @RequestMapping("/cargos")
@@ -35,8 +38,12 @@ public class CargoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar(ModelMap model) {
-		model.addAttribute("cargos", cargoService.buscarTodos());
+	public String listar(ModelMap model, @RequestParam("page") Optional<Integer> page) {
+		
+		int paginaAtual = page.orElse(1);
+		PaginacaoUtil<Cargo> pageCargo = cargoService.buscarPorPagina(paginaAtual);
+		
+		model.addAttribute("pageCargo", pageCargo);
 		return "cargo/lista";
 	}
 	
