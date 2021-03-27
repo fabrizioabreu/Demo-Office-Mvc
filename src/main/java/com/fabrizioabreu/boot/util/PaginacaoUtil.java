74134141d5ff32fd51226543e917a7478d0bde79
Fabrizio Abreu <fabrizioabreu@live.com>
2021-03-27 19:02:56 -0300

    Incluindo recurso de ordenação Ascendente e Descendente

--- src/main/java/com/fabrizioabreu/boot/util/PaginacaoUtil.java
+++ src/main/java/com/fabrizioabreu/boot/util/PaginacaoUtil.java
@@ -7,13 +7,15 @@ public class PaginacaoUtil<T> {
 	private int tamanho;
 	private int pagina;
 	private long totalDePaginas;
+	private String direcao;
 	private List<T> registros;
 	
-	public PaginacaoUtil(int tamanho, int pagina, long totalDePaginas, List<T> registros) {
+	public PaginacaoUtil(int tamanho, int pagina, long totalDePaginas, String direcao, List<T> registros) {
 		super();
 		this.tamanho = tamanho;
 		this.pagina = pagina;
 		this.totalDePaginas = totalDePaginas;
+		this.direcao = direcao;
 		this.registros = registros;
 	}
 
@@ -32,6 +34,8 @@ public class PaginacaoUtil<T> {
 	public List<T> getRegistros() {
 		return registros;
 	}
-	
-	
+
+	public String getDirecao() {
+		return direcao;
+	}
 }
